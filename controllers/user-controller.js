61cf609a7c1cf9f522bffeed4f93b7af1357886e
Tony Linz <tonyllinz@gmail.com>
2022-04-03 20:45:59 -0500

    update and delete user by id routes work. decided not to add two way friend adding

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -76,6 +76,33 @@ const userController = {
                 res.json(err);
             });
     },
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.userId }, body, { new: true })
+            .then((dbUserData) => {
+                if (!dbUserData) {
+                    return res.status(404).json({
+                        message: "No user found with this id",
+                    });
+                }
+                res.json(dbUserData);
+            })
+            .catch((err) => {
+                res.json(err);
+            });
+    },
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.userId })
+            .then((dbUserData) => {
+                if (!dbUserData) {
+                    res.status(404).json({
+                        message: "No user found with this id",
+                    });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch((err) => res.status(400).json(err));
+    },
 };
 
 module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,11 +6,13 @@ const {
     getUserById,
     addFriend,
     removeFriend,
+    updateUser,
+    deleteUser,
 } = require("../../controllers/user-controller");
 
 router.route("/").get(getAllUsers).post(createUser);
 
-router.route("/:userId").get(getUserById);
+router.route("/:userId").get(getUserById).put(updateUser).delete(deleteUser);
 
 router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 
